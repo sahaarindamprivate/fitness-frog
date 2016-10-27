66b850655595e3b3e580505eb4035b947de5f37e
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-10-27 09:33:20 -0500

    Remove and sort usings

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using Treehouse.FitnessFrog.Data;
 using Treehouse.FitnessFrog.Models;
